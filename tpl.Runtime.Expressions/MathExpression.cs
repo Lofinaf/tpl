ac8aff015c7ce5ddcd8b916c4b65c18c6bc5161e
profesianal <profesianalgay@mail.ru>
2023-01-15 22:22:44 +0300

    Expression added

---
+++ tpl.Runtime.Expressions/MathExpression.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using tpl.Runtime.Expressions;
+
+namespace tpl.Runtime.Expressions
+{
+    public unsafe sealed class MathExpression : IExpression
+    {
+        public Stack<string> Three { get; set; }
+
+        public int Analysis()
+        {
+            int ExprValueResult = 0;
+
+            foreach (var item in Three)
+            {
+
+            }
+
+            if (int.TryParse(Three.Pop(), out int Value))
+            {
+
+            }
+
+            return ExprValueResult;
+        }
+    }
+}
